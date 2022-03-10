7adbad1acaf544f182684a5c32d0e79ee4b3aba4
Owoade <63651250+Owoade@users.noreply.github.com>
2022-03-10 19:28:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,36 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore,combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import * as actions from './actions/';
-import { reveal } from './actions/actionMethods';
 
-const bgReducer = (state=localStorage.getItem("isDarkmode") == undefined ? "false" : localStorage.getItem("isDarkmode"), action)=>{
-   switch(action.type){
-     
-     case actions.DARK_MODE:
-       state="true"
-       localStorage.setItem("isDarkmode","true");
-       return state
-     break 
-     case actions.LIGHT_MODE :
-       state = "false";
-       localStorage.setItem("isDarkmode","false");
-       return state
-     break 
-     case actions.REVEAL:
-       return state
-      break    
-   }
-}
-const store = createStore(bgReducer);
-store.dispatch(reveal())
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
     <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
