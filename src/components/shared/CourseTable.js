1a9214ab59597beb9895ae7e1e10ba04ceea4f69
aycom366 <bamigboyeayomide200@gmail.com>
2022-03-14 16:36:07 +0100

    Fixed overflowing issues

--- src/components/shared/CourseTable.js
+++ src/components/shared/CourseTable.js
@@ -3,41 +3,43 @@ import {
   VStack,
   Tbody,
   Td,
-  Text,
   Th,
   Table,
   Thead,
   Tr,
+  Box,
 } from '@chakra-ui/react';
 
 const CourseTable = ({ semester, data }) => {
   return (
-    <VStack>
+    <VStack w="full" overflow={'hidden'}>
       <Heading size="sm">{semester}</Heading>
-      <Table w="full" size="sm">
-        <Thead>
-          <Tr>
-            {data[0].header.map((header, index) => (
-              <Th key={index}>{header}</Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data[0].rows.map((row, index) => (
-            <Tr key={index}>
-              <Td>{row.SN}</Td>
-              <Td>{row['Course Code']}</Td>
-              <Td minW={'200px'} w="300px">
-                {row['Course Title']}
-              </Td>
-              <Td minW={'20px'}>{row.L}</Td>
-              <Td minW={'20px'}>{row.P}</Td>
-              <Td minW={'20px'}>{row.CU}</Td>
-              <Td minW={'20px'}>{row.CH}</Td>
+      <Box w="full" overflowX="auto">
+        <Table w="full" size="sm">
+          <Thead>
+            <Tr>
+              {data[0].header.map((header, index) => (
+                <Th key={index}>{header}</Th>
+              ))}
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {data[0].rows.map((row, index) => (
+              <Tr key={index}>
+                <Td>{row.SN}</Td>
+                <Td>{row['Course Code']}</Td>
+                <Td minW={'200px'} w="300px">
+                  {row['Course Title']}
+                </Td>
+                <Td minW={'20px'}>{row.L}</Td>
+                <Td minW={'20px'}>{row.P}</Td>
+                <Td minW={'20px'}>{row.CU}</Td>
+                <Td minW={'20px'}>{row.CH}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
     </VStack>
   );
 };
